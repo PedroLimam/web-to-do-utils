9ca530179cebf2a9cf17ad0d6065aaab365a6ca5
Pedro Lima <pedro.lima@olivasdigital.com.br>
2024-03-08 16:30:53 -0300

    :sparkles: feat: Carregando páginas sob demanda, code Splitting com react lazy

--- src/Routes/appRoutes.tsx
+++ src/Routes/appRoutes.tsx
@@ -1,14 +1,19 @@
-import Login from "@/pages/Login/login";
-import NewPassword from "@/pages/NewPassword/newPassword";
-import Register from "@/pages/Register/register";
+import LoadingPage from '@/pages/LoadingPage/loadingPage';
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from "react-router-dom";
+
+const Login = lazy(() => import("@/pages/Login/login"));
+const NewPassword = lazy(() => import("@/pages/NewPassword/newPassword"));
+const Register = lazy(() => import("@/pages/Register/register"));
 
 export function AppRoutes(){
   return(
-    <Routes>
-      <Route path="/" element={<Login/>} />
-      <Route path="/register" element={<Register/>} />
-      <Route path="/change-password" element={<NewPassword/>} />
-    </Routes>
+    <Suspense fallback={<LoadingPage/>}>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/change-password" element={<NewPassword/>} />
+      </Routes>
+    </Suspense>
   )
 }
---
+++ src/pages/LoadingPage/loadingPage.tsx
@@ -0,0 +1,12 @@
+import { ContainerCenter as Container, Title } from "@/ui/styles/utils";
+
+export default function LoadingPage(){
+
+  return(
+    <Container>
+      <Title>
+        Carregando...
+      </Title>
+    </Container>
+  )
+}
